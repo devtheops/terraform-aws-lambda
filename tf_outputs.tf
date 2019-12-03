5d8f354fda2d7396b73be958ec97752f9697b29c
digitalfiz <mseiler@gmail.com>
2019-12-02 21:28:09 -0500

    Update tf_outputs.tf

--- tf_outputs.tf
+++ tf_outputs.tf
@@ -8,6 +8,11 @@ output "function_name" {
   description = "A unique name for your Lambda Function."
 }
 
+output "function_version" {
+  value       = aws_lambda_function.main.version
+  description = "Latest published version of your Lambda Function."
+}
+
 output "function_invoke_arn" {
   value       = aws_lambda_function.main.invoke_arn
   description = "The ARN to be used for invoking Lambda Function from API Gateway - to be used in (aws_api_gateway_integration)[https://www.terraform.io/docs/providers/aws/r/api_gateway_integration.html]'s uri"
